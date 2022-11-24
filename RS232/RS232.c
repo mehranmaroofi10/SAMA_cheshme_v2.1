5f753632c041e56a247cf6f1737979b854ca0416
mehran <81967090+mehranmaroofi10@users.noreply.github.com>
2022-11-24 22:41:10 +0330

    Mehran change

--- RS232/RS232.c
+++ RS232/RS232.c
@@ -4,11 +4,10 @@
 #include "main.h"
 #include "string.h"
 #include "RS232.h"
-#include "FirstTime_Controller.h"
+
+extern huart UART_HandleTypeDef huart4
 
 void RS232(char*data){
-	OLED_SHOW("RS232.............");
-	HAL_Delay(100);
-	//UART_HandleTypeDef huart4;
-	//HAL_UART_Transmit(UART_RS232 ,(uint8_t*)data,strlen(data),1000);
+
+	HAL_UART_Transmit(UART_RS232 ,(uint8_t*)data,strlen(data),1000);
 }
