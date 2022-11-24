64a3e730cdea27da48fc6972d1ca02fc8f1dc6a7
mehran <81967090+mehranmaroofi10@users.noreply.github.com>
2022-11-24 22:42:50 +0330

    Mehran change

--- RS232/RS232.h
+++ RS232/RS232.h
@@ -3,10 +3,7 @@
 #ifndef __RS232_H
 #define __RS232_H
 #include "stm32f4xx_hal.h"
-#define UART_RS232 &huart4
-#define RS232_ACTIVE HAL_UART_Transmit(&huart4,(uint8_t*)buffer,strlen(buffer),1000)
-
-
 
 void RS232(char*buffer);
+
 #endif
